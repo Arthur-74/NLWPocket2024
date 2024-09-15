e199ea6da6686680071fe48356538481c43254d0
Arthur-74 <arthurwilliamfb@hotmail.com>
2024-09-15 03:13:16 -0300

    Arrays e Objetos

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // Hello World
-console.log("Hello World!");
+let mensagem = "Hello World"
+mensagem = 2
+// console.log(mensagem);
+
+// Arrays, Objetos
+let metas = ["Arthur", "William"]
+console.log(metas[0] + " " + metas[1])
+
