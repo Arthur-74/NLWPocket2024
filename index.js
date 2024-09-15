d147e7c2114482a35dd90c9fa7f7bba0f9b5d88d
Arthur-74 <arthurwilliamfb@hotmail.com>
2024-09-15 03:41:00 -0300

    Funções

--- index.js
+++ index.js
@@ -5,5 +5,14 @@ mensagem = 2
 
 // Arrays, Objetos
 let metas = ["Arthur", "William"]
-console.log(metas[0] + " " + metas[1])
+// console.log(metas[0] + " " + metas[1])
 
+let meta = {
+    value: "ler um livro por mês",
+    checked: false,
+}
+console.log(meta.value)
+
+// Função //Arrow Function
+// function criarMeta() {}
+const criarMeta = () => {}
